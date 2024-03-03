9851513aa0d1081ecea3ddd8de3e4dd3e9cf795b
makenzix <118549889+makenzix@users.noreply.github.com>
2024-03-03 14:16:10 +0300

    zetsc.php

---
+++ zetsc.php
@@ -0,0 +1,14 @@
+{
+	"name":"Turksc",
+	"version":"0.2",
+	"author":"TurkscByjocker",
+	"classifiers":
+		{
+			"Programming_Language":"PHP",
+			"Operating System":
+				[
+					"Linux",
+					"Windows"
+				]
+		}
+}
